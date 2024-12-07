98bdde7f9d6cf3506a24c6002d6e902b2b9f92f9
Achmad Irianto Eka Putra <i@ryanbekhen.dev>
2024-12-08 06:38:27 +0700

    feat(tor): add tor bootstrap status check function
    Implemented a function to check the Tor bootstrap status and integrate it with the identity switching process. This ensures the system waits for Tor network readiness before issuing new identity requests, enhancing reliability.

--- pkg/tor/identity.go
+++ pkg/tor/identity.go
@@ -8,7 +8,62 @@ import (
 	"time"
 )
 
+func waitForTorBootstrap(logger *zerolog.Logger, timeout time.Duration) error {
+	complete := make(chan bool)
+
+	go func() {
+		for {
+			if checkTorBootstrapStatus() {
+				complete <- true
+				return
+			}
+			time.Sleep(5 * time.Second)
+		}
+	}()
+
+	select {
+	case <-complete:
+		logger.Info().Msg("Tor bootstrap selesai 100%")
+		return nil
+	case <-time.After(timeout):
+		return fmt.Errorf("timeout: Tor bootstrap tidak selesai dalam waktu %v", timeout)
+	}
+}
+
+func checkTorBootstrapStatus() bool {
+	conn, err := net.Dial("tcp", "127.0.0.1:9051")
+	if err != nil {
+		fmt.Println("Error connecting to Tor control port:", err)
+		return false
+	}
+	defer conn.Close()
+
+	_, err = conn.Write([]byte("GETINFO status/bootstrap-phase\r\n"))
+	if err != nil {
+		fmt.Println("Error sending command:", err)
+		return false
+	}
+
+	buffer := make([]byte, 2048)
+	_, err = conn.Read(buffer)
+	if err != nil {
+		fmt.Println("Error reading from Tor control port:", err)
+		return false
+	}
+
+	if string(buffer) == "250-status/bootstrap-phase=NOTICE BOOTSTRAP PROGRESS=100\r\n" {
+		return true
+	}
+
+	return false
+}
+
 func SwitcherIdentity(logger *zerolog.Logger, switchInterval time.Duration) {
+	if err := waitForTorBootstrap(logger, 5*time.Minute); err != nil {
+		logger.Error().Msg(err.Error())
+		return
+	}
+
 	for {
 		if err := requestNewTorIdentity(logger); err != nil {
 			logger.Error().Msg(err.Error())
@@ -18,7 +73,7 @@ func SwitcherIdentity(logger *zerolog.Logger, switchInterval time.Duration) {
 }
 
 func requestNewTorIdentity(logger *zerolog.Logger) error {
-	conn, err := net.Dial("tcp", "localhost:9051")
+	conn, err := net.Dial("tcp", "127.0.0.1:9051")
 	if err != nil {
 		return fmt.Errorf("failed to connect to tor control port: %w", err)
 	}
