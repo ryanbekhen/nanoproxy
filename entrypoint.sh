253bd26efa19b6d0b4c9d7c6d3545c05a12ee473
Achmad Irianto Eka Putra <i@ryanbekhen.dev>
2024-12-08 06:10:46 +0700

    fix: improve Tor bootstrap check reliability
    Enhanced the Tor bootstrap checking mechanism by optimizing the command sent to the control port. Changed the sleep interval from 1 second to 5 seconds for better efficiency and reduced load.

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,12 +3,11 @@
 wait_for_tor_bootstrap() {
     echo "Waiting for Tor to bootstrap..."
     while true; do
-        BOOTSTRAP_STATUS=$(printf 'GETINFO status/bootstrap-phase\nQUIT\n' | nc 127.0.0.1 9051 2>/dev/null | grep '100% (done)')
-        if [ -n "$BOOTSTRAP_STATUS" ]; then
-            echo "Tor has bootstrapped 100%"
-            break
+        if echo -e "AUTHENTICATE \"\"\r\nGETINFO status/bootstrap-phase\r\nQUIT\r\n" | nc 127.0.0.1 9051 2>/dev/null | grep -q '100% (done)'; then
+            echo "Tor selesai bootstrap."
+            break;
         fi
-        sleep 1
+        sleep 5
     done
 }
 
