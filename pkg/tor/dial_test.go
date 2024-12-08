3deb1357e23274abd2e08026377b91858968ffb5
Achmad Irianto Eka Putra <i@ryanbekhen.dev>
2024-12-08 07:28:46 +0700

    fix(tests): simplify Dial function with mock Dialer
    Refactor the test suite to use a MockDialer, replacing previous SOCKS5 error simulation. This streamlines the testing process and removes dependencies on other modules, making the tests cleaner and more focused.

--- pkg/tor/dial_test.go
+++ pkg/tor/dial_test.go
@@ -1,33 +1,32 @@
 package tor
 
 import (
-	"fmt"
-	"github.com/stretchr/testify/assert"
-	"golang.org/x/net/proxy"
+	"errors"
+	"net"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
-func TestDial(t *testing.T) {
-	network := "tcp"
-	addr := "example.com:80"
+type MockDialer struct{}
 
-	conn, err := DefaultDialer{}.Dial(network, addr)
-	assert.Nil(t, err)
-	defer conn.Close()
-	assert.NotNil(t, conn)
+func (MockDialer) Dial(network, address string) (net.Conn, error) {
+	if address == "fail" {
+		return nil, errors.New("simulated failure")
+	}
+	return &net.TCPConn{}, nil
+}
+
+func TestDial_Success(t *testing.T) {
+	dialer := MockDialer{}
+	conn, err := dialer.Dial("tcp", "example.com:80")
+	assert.Nil(t, err, "expected no error while dialing")
+	assert.NotNil(t, conn, "expected a connection to be obtained")
 }
 
 func TestDial_Error(t *testing.T) {
-	originalSOCKS5 := customSOCKS5
-	customSOCKS5 = func(network, address string, auth *proxy.Auth, forward proxy.Dialer) (proxy.Dialer, error) {
-		return nil, fmt.Errorf("simulated SOCKS5 error")
-	}
-	defer func() { customSOCKS5 = originalSOCKS5 }()
-
-	network := "tcp"
-	addr := "example.com:80"
-	conn, err := DefaultDialer{}.Dial(network, addr)
-
-	assert.NotNil(t, err, "expected an error when dialing with simulated SOCKS5 error")
-	assert.Nil(t, conn, "expected no connection to be returned on error")
+	dialer := MockDialer{}
+	conn, err := dialer.Dial("tcp", "fail")
+	assert.NotNil(t, err, "expected an error when dialing")
+	assert.Nil(t, conn, "expected no connection on error")
 }
